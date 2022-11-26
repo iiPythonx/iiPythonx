075b6718e92e5f7c42ac14f2b9893fcc493bfd3c
iiPython <ben@iipython.cf>
2022-11-26 16:55:07 -0600

    Update everything with the repo

--- dotfiles/launch_novnc.sh
+++
@@ -1,2 +0,0 @@
-x11vnc -rfbauth ~/.vnc/passwd -ncache 10 -display :0 -forever -loop -solid &
-cd ~/.novnc && ./utils/novnc_proxy --vnc localhost:5900
---
+++ dotfiles/screenshot.sh
@@ -0,0 +1,1 @@
+gnome-screenshot -acf /tmp/screenshot && cat /tmp/screenshot | xclip -i -selection clipboard -target image/png
