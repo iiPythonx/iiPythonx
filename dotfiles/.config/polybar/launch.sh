b04752bc2d6f4fce119cbeb7764d19e7d8885313
iiPython <ben@iipython.dev>
2023-08-27 18:17:10 -0500

    Update dotfiles to 2023+

--- dotfiles/.config/polybar/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -q main -c "$HOME/.config/polybar/config.ini" &
--- dotfiles/screenshot.sh
+++
@@ -1,1 +0,0 @@
-gnome-screenshot -acf /tmp/screenshot && cat /tmp/screenshot | xclip -i -selection clipboard -target image/png
