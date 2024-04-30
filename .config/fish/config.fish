071aab0941ed19578bd2735385fb5f831e937e07
iiPython <ben@iipython.dev>
2024-04-30 11:17:03 -0500

    Update dotfiles (just fish config really)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,13 @@
 set fish_greeting
+set -g -x EDITOR micro
+
 starship init fish | source
 
 fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+
+alias v "source .venv/bin/activate.fish"
+alias ls "eza"
+alias nano "micro"
 
 eval $(keychain --eval id_ed25519 -q --noask)
