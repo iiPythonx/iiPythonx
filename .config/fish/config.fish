543106a203f2bb8a13dc8dfe8f1d1d6039487c8f
iiPython <ben@iipython.dev>
2024-08-21 16:17:14 -0500

    add bun to path, new wallpaper, hide autoinstalled fonts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,7 @@ starship init fish | source
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.rye/shims
+fish_add_path ~/.bun/bin
 
 alias v "source .venv/bin/activate.fish"
 alias ls "eza"
