2c923a17d919854a59af1212226eff23baf9330d
iiPython <ben@iipython.dev>
2024-09-06 22:34:14 -0500

    remove useless fish paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,6 @@ set -g -x EDITOR micro
 
 starship init fish | source
 
-fish_add_path ~/.local/bin
-fish_add_path ~/.cargo/bin
-fish_add_path ~/.rye/shims
-fish_add_path ~/.bun/bin
-
 alias v "source .venv/bin/activate.fish"
 alias ls "eza"
 alias nano "micro"
