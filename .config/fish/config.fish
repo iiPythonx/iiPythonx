435ee9e1ae467ec515285b00723fafa6dca29ce2
iiPython <ben@iipython.dev>
2024-12-29 13:53:49 -0600

    push

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,14 @@
 set fish_greeting
+
 set -g -x EDITOR micro
+set -g -x QT_QPA_PLATFORMTHEME qt5ct
 
 starship init fish | source
 
 fish_add_path "/home/benjamin/.bun/bin"
+fish_add_path "/home/benjamin/.venv/bin"
 
-alias v "source .venv/bin/activate.fish"
+alias vv "source .venv/bin/activate.fish"
 alias ls "eza"
 alias nano "micro"
 
