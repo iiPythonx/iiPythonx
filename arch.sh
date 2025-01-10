32b2205b0732e4990fda409490036f485ee74e48
iiPython <ben@iipython.dev>
2025-01-10 16:02:08 -0600

    hopefully final change

--- arch.sh
+++ arch.sh
@@ -16,6 +16,9 @@
 sudo systemctl enable --now NetworkManager
 sleep 5
 
+# Ensure system is up to date (it should be, but just in case)
+sudo pacman -Syu --noconfirm
+
 # Install our desktop
 sudo pacman -S --noconfirm ly labwc swaybg waybar kanshi
 sudo systemctl enable ly
@@ -36,20 +39,32 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
 sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
+sudo tee -a /etc/pacman.conf <<EOL
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOL
+
 sudo pacman -Syy --noconfirm yay-git
 
 # Install basic desktop applications
-sudo pacman -S --noconfirm thunderbird foot nemo python micro starship eza uv keychain rofi-wayland nicotine+ mpv easyeffects uv bun keepassxc
+sudo pacman -S --noconfirm thunderbird foot nemo python micro starship eza uv keychain rofi-wayland nicotine+ mpv easyeffects bun keepassxc age
 sudo ln -s /usr/bin/micro /usr/bin/nano
-yay -S --noconfirm iipython-feishin-bin jellyfin-media-player
+yay -S --noconfirm iipython-feishin-bin jellyfin-media-player syncthingtray-qt6 vesktop-bin
+
+# Handle syncing
+mkdir -p ~/.sync
+
+# Audio
+sudo pacman -S pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber
+
+# Handle SSH
+# This expects the keyfile to already be in place (copied through the arch iso, etc)
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_ed25519
 
 # Create global python venv
 uv venv
 uv pip install git+https://github.com/iiPythonx/usps git+https://github.com/nathom/streamrip
 
-# Setup theme
-yay -S --noconfirm mint-themes
-sudo pacman -S glib2 python-gobject
-gsettings set org.gnome.desktop.interface gtk-theme "Mint-Y-Dark"
-curl -O https://raw.githubusercontent.com/labwc/labwc-gtktheme/refs/heads/master/labwc-gtktheme.py
-python3 labwc-gtktheme.py && rm labwc-gtktheme.py
+# And we're done
+echo "This system has been iiPython-ified. Please reboot as soon as possible."
