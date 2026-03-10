7571b8854efe09aaf65b3c5ee3df620d65e403f3
iiPython <ben@iipython.dev>
2026-03-10 01:08:29 -0500

    2026 update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,14 @@ set fish_greeting
 set -g -x EDITOR micro
 set -g -x QT_QPA_PLATFORMTHEME qt6ct
 
+if test -d /backup
+    set -g -x UV_CACHE_DIR /backup/.uv
+end
+
 starship init fish | source
 
 fish_add_path "/home/benjamin/.bun/bin"
+fish_add_path "/home/benjamin/.local/bin"
 fish_add_path "/home/benjamin/.venv/bin"
 
 alias vv "source .venv/bin/activate.fish"
